2890cd08aed7b47a926cf0a5194438ec8dc7bca4
Dhananjay Mane <dhananjay.mane@mobisoftinfotech.com>
2019-01-30 10:18:42 +0530

    Added control to add dynamic values

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -5,17 +5,25 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
     const transformedIngredient = Object.keys(props.ingredients);
-    const ingredientHtml = transformedIngredient.map(ingredient => {
-        return [...Array(props.ingredients[ingredient])].
-            map((_, i) => <BurgerIngredient type={ingredient} />);
-    });
+    let ingredientHtml = transformedIngredient
+        .map(ingredient => {
+            return [...Array(props.ingredients[ingredient])]
+                .map((_, i) => <BurgerIngredient key={i} type={ingredient} />);
+        })
+        .reduce((arr, el) => arr.concat(el), []);
+
+    if (!ingredientHtml.length) {
+        ingredientHtml = <p>Please add ingredients</p>;
+    }
 
     return (
-        <div className={classes.Burger}>
-            <BurgerIngredient type='bread-top' />
-            {ingredientHtml}
-            <BurgerIngredient type='bread-bottom' />
-        </div>
+        <>
+            <div className={classes.Burger}>
+                <BurgerIngredient type='bread-top' />
+                {ingredientHtml}
+                <BurgerIngredient type='bread-bottom' />
+            </div>
+        </>
     );
 }
 
--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 
 import Burger from '../../Components/Burger/Burger';
+import BuildControls from '../../Components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
         }
     }
 
     render() {
         return (
-            <Burger ingredients={this.state.ingredients} />
+            <>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls />
+            </>
         );
     }
 }
